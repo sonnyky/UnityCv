96f9b3d4aa0b963f64b347328bfc56936761b7e6
sonny <sonny.kurniawan.yap@gmail.com>
2019-10-08 17:11:33 +0800

    first commit

---
+++ Assets/App/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+using System.Runtime.InteropServices;
+using System;
+
+/*
+        Controller provides examples for passing pointers for byte arrays back and forth
+        between managed C# and native C++. When making your own classes, be sure to specify
+        the Texture2D texture format before attempting to load raw bytes into it.
+    */
+public class Controller : MonoBehaviour
+{
+
+	public string m_ImagePath = "Image";
+
+	void Awake()
+	{
+		Debug.LogWarning("Test Value in C++ is " + NativeLibAdapter.Test());
+
+			Debug.Log ("Saving image...");
+			NativeLibAdapter.SaveBlackAndWhite (m_ImagePath);
+		
+	}
+
+	void Update()
+	{
+	}
+}
---
+++ Assets/App/Scripts/NativeLibAdapter.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+using UnityEngine;
+
+/*
+        NativeLibAdapter is an example communication layer between managed C# and native C++
+    */
+public class NativeLibAdapter
+{
+	#if !UNITY_EDITOR
+	/*
+	[DllImport("native-lib")]
+	private static extern int TestFunction_Internal();
+	*/
+	#elif UNITY_EDITOR
+	[DllImport("UnityCvPlugin")]
+	private static extern int TestFunction_Internal();
+
+	[DllImport("UnityCvPlugin")]
+	private static extern void _SaveBlackAndWhite(IntPtr bytes, int rows, int cols, int type);
+
+
+	#endif
+
+
+	public static int Test()
+	{
+		#if !UNITY_EDITOR
+		return TestFunction_Internal();
+		#elif UNITY_EDITOR
+		return TestFunction_Internal();
+		#else
+		return -1;
+		#endif
+	}
+
+	public static void SaveBlackAndWhite(string imagePath)
+	{
+
+		byte[] imageBytes = System.IO.File.ReadAllBytes(imagePath);
+		Texture2D tmpTexture = new Texture2D(1, 1);
+		tmpTexture.LoadImage(imageBytes);
+		Color32[] pixelData = tmpTexture.GetPixels32();
+
+		GCHandle pixelHandle = GCHandle.Alloc(pixelData, GCHandleType.Pinned);
+		IntPtr pixelPtr = pixelHandle.AddrOfPinnedObject();
+
+		_SaveBlackAndWhite(pixelPtr, 334, 500, 1);
+	}
+
+}
