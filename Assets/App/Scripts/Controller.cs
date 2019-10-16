58b94b19640f5fc105c3f797b0f57dca4f293058
sonny <sonny.kurniawan.yap@gmail.com>
2019-10-16 14:07:49 +0800

    画像の比較テストサンプル

--- Assets/App/Scripts/Controller.cs
+++ Assets/App/Scripts/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 using System.Runtime.InteropServices;
 using System;
@@ -12,15 +13,47 @@ using System;
 public class Controller : MonoBehaviour
 {
 
+	public string m_FolderPath = "";
 	public string m_ImagePath = "Image";
+	public string m_ImageToCompare = "ToCompare";
 
 	void Awake()
 	{
 		Debug.LogWarning("Test Value in C++ is " + NativeLibAdapter.Test());
 
-			Debug.Log ("Saving image...");
-			NativeLibAdapter.SaveBlackAndWhite (m_ImagePath);
-		
+		Debug.Log ("Test float : " + NativeLibAdapter.TestFloat());
+			//NativeLibAdapter.SaveBlackAndWhite (m_ImagePath);
+
+	}
+
+	void Start(){
+
+		float timeNow = Time.realtimeSinceStartup;
+		/*
+		string[] filePaths= Directory.GetFiles(m_FolderPath,"*.jpg");
+
+		for (int i = 0; i < filePaths.Length; i++) {
+			float similarity = NativeLibAdapter.GetImageStructureSimilarity (m_ImagePath, filePaths[i]);
+			string result = " is different";
+			if(similarity < 0.0001f) result = " is same";
+			Debug.Log ("The image : " + filePaths[i] + " and similarity to ref : " + similarity + result);
+		}
+*/
+
+		float similarity = NativeLibAdapter.GetImageStructureSimilarity (m_ImagePath, m_ImageToCompare, 2);
+		string result = " is different";
+		if(similarity < 0.0001f) result = " is same";
+		Debug.Log ("similarity : " + similarity + result);
+
+		/*
+		float similaritySift = NativeLibAdapter.GetImageSiftSimilarity (m_ImagePath, m_ImageToCompare);
+		string resultSift = " is different";
+		if(similaritySift < 0.0001f) resultSift = " is same";
+		Debug.Log ("similarity with SIFT : " + similaritySift + resultSift);
+*/
+		float timeAfterMethod = Time.realtimeSinceStartup;
+
+		Debug.Log ("Elapsed time : " + (timeAfterMethod - timeNow));
 	}
 
 	void Update()
--- Assets/App/Scripts/NativeLibAdapter.cs
+++ Assets/App/Scripts/NativeLibAdapter.cs
@@ -20,6 +20,14 @@ public class NativeLibAdapter
 	[DllImport("UnityCvPlugin")]
 	private static extern void _SaveBlackAndWhite(IntPtr bytes, int rows, int cols, int type);
 
+	[DllImport("UnityCvPlugin")]
+	private static extern float _TestGetImageShapeSimilarity();
+
+	[DllImport("UnityCvPlugin")]
+	private static extern float _CompareStructureSimilarity(IntPtr bytesRef, IntPtr toCompare, int rows, int cols, int type);
+
+	[DllImport("UnityCvPlugin")]
+	private static extern float _CompareSimilarityWithFeatures(IntPtr bytesRef, IntPtr toCompare, int rows, int cols, int type);
 
 	#endif
 
@@ -47,6 +55,82 @@ public class NativeLibAdapter
 		IntPtr pixelPtr = pixelHandle.AddrOfPinnedObject();
 
 		_SaveBlackAndWhite(pixelPtr, 334, 500, 1);
+	}
+
+	public static float TestFloat ()
+	{
+		float oneData = _TestGetImageShapeSimilarity ();
+		return oneData;
+	}
+
+	public static float GetImageStructureSimilarity(string imgPath, string imgCompare, int type){
+		byte[] imageBytes = System.IO.File.ReadAllBytes(imgPath);
+		Texture2D tmpTexture = new Texture2D(1, 1);
+		tmpTexture.LoadImage(imageBytes);
+
+		Texture2D resizedRef = Resize (tmpTexture, 3515, 2490);
+
+		Color32[] pixelData = resizedRef.GetPixels32();
+
+		GCHandle pixelHandle = GCHandle.Alloc(pixelData, GCHandleType.Pinned);
+		IntPtr pixelPtr = pixelHandle.AddrOfPinnedObject();
+
+		byte[] imageCompareBytes = System.IO.File.ReadAllBytes(imgCompare);
+		Texture2D cmpTexture = new Texture2D(1, 1);
+		cmpTexture.LoadImage(imageCompareBytes);
+
+		Texture2D resizedCompareRef = Resize (cmpTexture, 3515, 2490);
+
+		Color32[] pixelCompareData = resizedCompareRef.GetPixels32();
+
+		GCHandle pixelCompareHandle = GCHandle.Alloc(pixelCompareData, GCHandleType.Pinned);
+		IntPtr pixelComparePtr = pixelCompareHandle.AddrOfPinnedObject();
+
+		float result = _CompareStructureSimilarity (pixelPtr, pixelComparePtr, 2490, 3515, type);
+
+		return result;
+	}
+
+	public static float GetImageSiftSimilarity(string imgPath, string imgCompare){
+		byte[] imageBytes = System.IO.File.ReadAllBytes(imgPath);
+		Texture2D tmpTexture = new Texture2D(1, 1);
+		tmpTexture.LoadImage(imageBytes);
+
+		Texture2D resizedRef = Resize (tmpTexture, 3515, 2490);
+
+		Color32[] pixelData = resizedRef.GetPixels32();
+
+		GCHandle pixelHandle = GCHandle.Alloc(pixelData, GCHandleType.Pinned);
+		IntPtr pixelPtr = pixelHandle.AddrOfPinnedObject();
+
+		byte[] imageCompareBytes = System.IO.File.ReadAllBytes(imgCompare);
+		Texture2D cmpTexture = new Texture2D(1, 1);
+		cmpTexture.LoadImage(imageCompareBytes);
+
+		Texture2D resizedCompareRef = Resize (cmpTexture, 3515, 2490);
+
+		Color32[] pixelCompareData = resizedCompareRef.GetPixels32();
+
+		GCHandle pixelCompareHandle = GCHandle.Alloc(pixelCompareData, GCHandleType.Pinned);
+		IntPtr pixelComparePtr = pixelCompareHandle.AddrOfPinnedObject();
+
+		float result = _CompareSimilarityWithFeatures (pixelPtr, pixelComparePtr, 2490, 3515, 1);
+
+		return result;
+	}
+
+	public static Texture2D Resize(Texture2D source, int newWidth, int newHeight)
+	{
+		source.filterMode = FilterMode.Point;
+		RenderTexture rt = RenderTexture.GetTemporary(newWidth, newHeight);
+		rt.filterMode = FilterMode.Point;
+		RenderTexture.active = rt;
+		Graphics.Blit(source, rt);
+		Texture2D nTex = new Texture2D(newWidth, newHeight);
+		nTex.ReadPixels(new Rect(0, 0, newWidth, newWidth), 0, 0);
+		nTex.Apply();
+		RenderTexture.active = null;
+		return nTex;
 	}
 
 }
